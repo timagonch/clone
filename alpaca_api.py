d8b28b736e99761ad75b44e707335584e4d7cc03
“Z” <jmechka@gmail.com>
2023-12-17 12:00:54 -1000

    latest

--- alpaca_api.py
+++ alpaca_api.py
@@ -4,9 +4,9 @@ from alpaca.trading.requests import MarketOrderRequest
 from alpaca.trading.stream import TradingStream
 import config
 
-client = TradingClient(config.API_KEY, config.SECRET_KEY, paper = True)
+# client = TradingClient(config.API_KEY, config.SECRET_KEY, paper = True)
 
-account = dict(client.get_account())
+# account = dict(client.get_account())
 
-for k,v in account.items():
-    print(f"{k:30}{v}")
+# for k,v in account.items():
+#     print(f"{k:30}{v}")
