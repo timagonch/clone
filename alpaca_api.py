b3bad9e17ab159d0ffdbf4584a4e4f785f2224a8
“Z” <jmechka@gmail.com>
2023-12-17 11:34:52 -1000

    main executable file

---
+++ alpaca_api.py
@@ -0,0 +1,12 @@
+from alpaca.trading.client import TradingClient
+from alpaca.trading.enums import OrderSide, TimeInForce
+from alpaca.trading.requests import MarketOrderRequest
+from alpaca.trading.stream import TradingStream
+import config
+
+client = TradingClient(config.API_KEY, config.SECRET_KEY, paper = True)
+
+account = dict(client.get_account())
+
+for k,v in account.items():
+    print(f"{k:30}{v}")
