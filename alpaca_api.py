2c70898f6495be61350bba457e45f22395d7c0d8
“Z” <jmechka@gmail.com>
2023-12-17 11:49:24 -1000

    new keys

--- alpaca_api.py
+++ alpaca_api.py
@@ -8,5 +8,5 @@ client = TradingClient(config.API_KEY, config.SECRET_KEY, paper = True)
 
 account = dict(client.get_account())
 
-for k,v in account.items():
-    print(f"{k:30}{v}")
+#for k,v in account.items():
+#    print(f"{k:30}{v}")
