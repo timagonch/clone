b82e3a8bf9d844620f3fdfaa81ed0b243370697c
“Z” <jmechka@gmail.com>
2023-12-17 13:38:46 -1000

    cleaned up

--- save_to_csv.py
+++ save_to_csv.py
@@ -4,7 +4,7 @@ import pandas as pd
 sp500_data = pd.read_pickle('sp500_data.pkl')
 
 # Define the file path where you want to save the CSV file
-file_path = '/Users/lilygoncharov/Documents/Z/sp500_historical_data.csv'
+file_path = '/Users/lilygoncharov/Documents/Z/data/sp500_historical_data.csv'
 
 # Save the DataFrame containing historical data to a CSV file
 sp500_data.to_csv(file_path)
