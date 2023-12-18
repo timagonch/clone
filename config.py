f52398303f23da6deb00bab43376fd0558c857dc
“Z” <jmechka@gmail.com>
2023-12-17 20:21:18 -1000

    latest

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 # ALPACA Keys
 
-API_KEY = 'PK6BG8Z29QYTCK1C4EMD'
+ALPACA_PAPER_API_ACCESS_KEY = 'PK6BG8Z29QYTCK1C4EMD'
 SECRET_KEY = '8YVgyawkNvdtw3FF4CKSHXwfwIF9CRyEXEmDamye'
 BASE_URL='https://paper-api.alpaca.markets'
 
-headers = {
-    'APCA-API-KEY-ID': API_KEY,
-    'APCA-API-SECRET-KEY': SECRET_KEY,
-}
+# headers = {
+#     'APCA-API-KEY-ID': API_KEY,
+#     'APCA-API-SECRET-KEY': SECRET_KEY,
+# }
 
 symbol = 'AAPL'
 # Endpoint URL for fetching historical bars
