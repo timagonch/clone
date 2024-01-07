90d3f580aa390f7fd795d5db19f2ddf9c9f6423d
timagonch <Tjgoncharov@gmail.com>
2024-01-07 13:59:14 -0500

    Timon is here

--- timon/first_python_code.py
+++ timon/first_python_code.py
@@ -1,1 +1,5 @@
 # hey timon, here you can play with your code
+
+
+# Got the environment set up, had to clone the github files into VS Code, not sure if that's the best way to do it, but let me know if you 
+# can see these changes
