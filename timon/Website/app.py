c44782ccddae52f762587207f3c621f99221c16c
timagonch <Tjgoncharov@gmail.com>
2024-01-14 23:30:02 -0500

    made a graph in flask

--- timon/Website/app.py
+++ timon/Website/app.py
@@ -1,11 +1,11 @@
-#firts install flask with pip install Flask
+#firts install flask with pip install Flask, plotly with pip install plotly, pandas with pip install pandas
 
 from flask import Flask
-from views import views
+from views import bp
 
 #initialize the app
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix="/")
+app.register_blueprint(bp, url_prefix="/")
 
 #automatically reload the app when code changes
 if __name__ == '__main__':
